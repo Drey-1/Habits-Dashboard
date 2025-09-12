5e1320010b584981ade7ce0ed5b9803027477551
Drey-1 <matosandrey11@gmail.com>
2025-09-12 16:04:31 -0300

    feat: add Dashboard component for habit tracking and time management

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import "../styles/Dashboard.css"
+
+type habitType = {
+  id: string,
+  title: string,
+  done: boolean,
+};
+
+type DashboardProps = {
+    items: habitType[]
+}
+
+export default function Dashboard({items}:DashboardProps) {
+    const totalItms = items.length;
+    const doneItms = items.filter(item => item.done === true).length;
+    const date = new Date();
+    const minutes = 60 - date.getMinutes();
+    const hours = (minutes != 0?23:24) - date.getHours();
+    
+
+    useEffect(() => {
+        if (doneItms === totalItms && totalItms > 0) {
+    window.alert("CONGRATULATIONS! You made all of your habits today.");}}, [doneItms, totalItms]);
+
+    return (
+        <div className="div-dash">
+            <span>Items dones: <b style={{color:"green"}}>{doneItms}</b>/{totalItms}</span>
+            <span>Remaining time: {hours}h and {minutes}min</span>
+        </div>
+    )
+}
