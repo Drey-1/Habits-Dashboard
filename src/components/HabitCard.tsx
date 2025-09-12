c16dd0b2bd98dc70d03addea304ac38295f51efc
Drey-1 <matosandrey11@gmail.com>
2025-09-12 16:53:12 -0300

    refactor: simplify Checking function and remove unnecessary event handling in HabitCard component

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -1,7 +1,7 @@
 import { Check, Trash } from "lucide-react"
 import Btn from "./Btn"
 import "../styles/HabitCard.css"
-import React, { useState } from "react";
+import { useState } from "react";
 
 type habitType = {
   id: string,
@@ -20,18 +20,14 @@ export default function HabitCard({ item, onRemove , onDone }:HabitCardProps) {
     const { title, id ,done } = item;
     const [ check, setCheck ] = useState(done)
 
-    const Checking = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const Checking = () => {
         setCheck( true );
-        e.currentTarget.className = "check-btn checked"
         onDone(id);
-        if(e.currentTarget.parentElement) {
-            e.currentTarget.parentElement.className = "checkedP"
-        }
     }
 
     return(
         <div className="card">
-            <div className="uncheckP">
+            <div className={ check? "checkedP":"uncheckP"}>
                 <button className={check ? "check-btn checked" : "check-btn"} onClick={Checking}>
                     <Check className="icon"/>
                 </button>
