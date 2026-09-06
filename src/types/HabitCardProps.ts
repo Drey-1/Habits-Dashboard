35001271d2e7bbe016730dd015ada626567e696b
Drey-1 <matosandrey11@gmail.com>
2026-09-06 15:50:48 -0300

    refactor: update HabitCardProps type definition for consistency

--- src/types/HabitCardProps.ts
+++ src/types/HabitCardProps.ts
@@ -1,10 +1,14 @@
 import type { Habit } from "./habitType";
 
 type HabitCardProps = {
-    item: Habit;
-    onRemove: (id: string) => void;
-    onDone: (id:string) => void;
-    onUpdate: (id:string, newTitle:string) => void;
-}
+  item: Habit;
+  onRemove: (id: string) => void;
+  onDone: (id: string) => void;
+  onUpdate: (id: string, newTitle: string) => void;
+  onDragStart: (id: string) => void;
+  onMouseOver: (e: React.DragEvent) => void;
+  onDrop: (idTarget: string) => void;
+  onDragEnd: () => void;
+};
 
-export type {HabitCardProps}
+export type { HabitCardProps };
