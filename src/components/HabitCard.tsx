81e6745315e4bac1ce46db641043288b8b4750c5
Drey-1 <matosandrey11@gmail.com>
2026-09-06 15:52:19 -0300

    refactor: enhance HabitCard component with drag-and-drop functionality

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -1,39 +1,47 @@
-import { Check, Trash, SquarePen } from "lucide-react"
-import Btn from "./Btn"
-import "../styles/HabitCard.css"
-import { useState } from "react";
+import { Check, Trash, SquarePen } from "lucide-react";
+import Btn from "./Btn";
+import "../styles/HabitCard.css";
 import type { HabitCardProps } from "../types/HabitCardProps";
 
-export default function HabitCard({ item, onRemove , onDone, onUpdate }:HabitCardProps) {
-    const { title, id ,done } = item;
-    const [ check, setCheck ] = useState(done)
+export default function HabitCard({ 
+    item, 
+    onRemove, 
+    onDone, 
+    onUpdate, 
+    onDragEnd, 
+    onDragStart, 
+    onMouseOver, 
+    onDrop 
+}: HabitCardProps) {
+    const { title, id, done } = item;
 
-    const Checking = () => {
-        setCheck( true );
-        onDone(id);
-    }
-
-    return(
-        <div className="card">
-            <div className={ check? "checkedP":"uncheckP"}>
-                <button className={check ? "check-btn checked" : "check-btn"} onClick={Checking}>
+    return (
+        <div 
+            className="card" 
+            draggable 
+            onDragStart={() => onDragStart(id)}
+            onDragOver={onMouseOver}
+            onDrop={() => onDrop(id)}
+            onDragEnd={onDragEnd}
+        >
+            <div className={done ? "checkedP" : "uncheckP"}>
+                <button className={done ? "check-btn checked" : "check-btn"} onClick={() => onDone(id)}>
                     <Check className="icon"/>
                 </button>
             </div>
             <div className="text">
                 {title}
                 <div className="updt" onClick={() => {
-                    const newTitle = window.prompt( "Write the new text:", item.title );
-                    if(!newTitle) return;
-                    onUpdate( id, newTitle )
-
+                    const newTitle = window.prompt("Write the new text:", item.title);
+                    if (!newTitle) return;
+                    onUpdate(id, newTitle);
                 }}>
                     <SquarePen />
                 </div>
             </div>
             <div onClick={() => onRemove(id)} style={{ display: 'inline-block' }}>
-                <Btn  color={"#be4040"} content={<Trash/>} />
+                <Btn color={"#be4040"} content={<Trash/>} />
             </div>   
         </div>
-    )
+    );
 }
